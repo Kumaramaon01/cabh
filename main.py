a64e765ae16b67167a7ca91fa97536bcc1064b6c
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-09-28 05:13:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,6 @@ st.markdown("""
         </style>
     """, unsafe_allow_html=True)
 
-
-# Initialize session state for script_choice if it does not exist
 if 'script_choice' not in st.session_state:
     st.session_state.script_choice = "people"  # Set default to "about"
 
